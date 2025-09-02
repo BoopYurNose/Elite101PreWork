3a0544ab83ff255e2fab3e447b737e6bfdf0a68b
BoopYurNose <zacharybaca07@gmail.com>
2025-09-02 23:41:22 +0000

    :wq

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+print("Welcome user!")
+NameInput = input("What is your name?")
+print(f"Hello {NameInput}")
+AgeInput = input("What is your Age?")
+print("How can I help you I can help you with \n Mathmatics \n Programming \n General Advice \n and More! \n Type Quit to end the conversation")
+UserInput = input()
+if UserInput == "Quit":
+    print("Alright then. Goodbye for now!")
