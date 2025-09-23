45ac45f6888abe93ddc131e43db2218bb500e0e6
BoopYurNose <zacharybaca07@gmail.com>
2025-09-23 22:17:06 +0000

    :wq

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+UserValue = False
+
 print("Welcome user!")
 NameInput = input("What is your name?")
 print(f"Hello {NameInput}")
 AgeInput = input("What is your Age?")
-print("How can I help you I can help you with \n Mathmatics \n Programming \n General Advice \n and More! \n Type Quit to end the conversation")
-UserInput = input()
-if UserInput == "Quit":
-    print("Alright then. Goodbye for now!")
+
+while UserValue == False:
+    print("How can I help you I can help you with \n Mathmatics \n Programming \n General Advice \n and More! \n Type Quit to end the conversation")
+    UserInput = input()
+    if UserInput == "Quit":
+        print("Alright then. Goodbye for now!")
+        UserValue = True
